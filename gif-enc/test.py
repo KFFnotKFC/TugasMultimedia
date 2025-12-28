da73a17671d1c447a316915b3445f635a7a00688
Penny_W1S3 <rafipndr@gmail.com>
2025-12-28 17:32:07 +0700

    Porting code to node js

--- gif-enc/test.py
+++
@@ -1,7 +0,0 @@
-import ppm
-import gif
-
-rgb_pixels = ppm.loadppm(open("tests/cat256.ppm", "rb"))
-indices, palette = gif.palettise(rgb_pixels)
-
-gif.save(open("tests/cat.gif", "wb"), indices, palette)
